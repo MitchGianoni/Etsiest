c41b5fc46a93e5e363abc38fc8ef62a6973526d2
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-21 03:08:35 -0400

    does not do anything

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,26 @@
 require "etsiest/version"
+require "sinatra/base"
+require "etsy"
+require "pry"
+
+Etsy.api_key = ENV["ETSY_KEY"] # this is where we pull the api key from the command line
 
 module Etsiest
-  # Your code goes here...
+  class Etsiest < Sinatra::Application
+  	get "/search" do
+		query = params["q"]
+		# Here is wehre we get data from Etsy's API
+		response  ETSY::Request.get("/listings/active", includes: ["Images", "Shop"], keywords: query)
+		# Here is where we pass that data to the ERB template to be turned into HTML  		
+		# NOTE how we call ".result" on the response from Etsy
+		# The Etsy gem returns a Etsy::Request object, not a hash
+		# To be able to get the *data* we want and loop over it
+		# in the ERB template, I need to pull the data out of the
+		# Etsy::Request instance. This "result" method works best.
+		erb :index, locals: { items: response.result }
+	end
+
+	run! uf app_file == $0
+
+  	end
 end
